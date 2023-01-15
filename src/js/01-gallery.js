6aea8db2eebd86e7ea74bd2eebab37551115da65
Stanislav <stas1que@ukr.net>
2023-01-15 15:10:05 +0100

    cccc

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createMarkup = galleryItems
+  .map(
+    item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      title="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', createMarkup);
+gallery.addEventListener('click', onclick);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionData: 'title',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import '../css/common.css';
+import Player from '@vimeo/player';
+import { throttle } from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const time = data.seconds;
+    console.log(time);
+    localStorage.setItem(TIME_DATA_PLAYER, time);
+  }, 1000)
+);
