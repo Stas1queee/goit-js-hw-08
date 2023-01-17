7991dcc226df57ab4ad6269050556316db19132a
Stanislav <stas1que@ukr.net>
2023-01-17 17:25:27 +0100

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,37 @@
 import  throttle  from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInput, 1000));
+
+populateMessage();
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateMessage() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    if (savedData.email) {
+      email.value = savedData.email;
+    }
+    if (savedData.message) {
+      message.value = savedData.message;
+    }
+  }
+}
